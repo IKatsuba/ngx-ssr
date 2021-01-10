caafe43ce6e059cbcb2dbd86db536ea3bd319393
Igor Katsuba <i.katsuba@tinkoff.ru>
2021-01-10 21:31:37 +0300

    initial commit

---
+++ apps/rickandmorty/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-ssr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'rickandmorty';
+}
